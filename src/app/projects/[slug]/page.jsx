79200d2720a9864a00710e14d31121817bafdd4e
Pradeep <prs.axios@gmail.com>
2025-06-26 12:55:44 +0530

    conditional data rendering

--- src/app/projects/[slug]/page.jsx
+++ src/app/projects/[slug]/page.jsx
@@ -1,15 +1,18 @@
 "use client";
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import style from "./style.module.scss";
 import data from "../../../data/projects.json";
 import { useParams, notFound } from 'next/navigation';
 import Image from 'next/image';
 import gsap from 'gsap';
-import { useGSAP } from "@gsap/react";
+import { TextPlugin } from 'gsap/TextPlugin';
+
+
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 // Register ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(TextPlugin);
 
 const HorizontalScroll = () => {
     const params = useParams();
@@ -21,35 +24,76 @@ const HorizontalScroll = () => {
 
     const { title, description, image_1, image_2, techStack } = project;
 
+    const textRef = useRef(null);
+
+    useEffect(() => {
+        gsap.to(textRef.current, {
+            duration: 2,
+            text: {
+                value: `${title}`,
+                scrambleText: {
+                    chars: "01",
+                    revealDelay: 0.1,
+                    speed: 0.1,
+                },
+            },
+            ease: "power2.out",
+
+        });
+    }, []);
+
 
 
     return (
         <div className={style.parentDiv} ref={containerRef}>
             <div className={style.body}>
-                {/* Panel One */}
-                <div className={style.panelOne}>
-                    <p>{title}</p>
-                    <p>{description}</p>
-                </div>
+                {/* Hero */}
+                <section className={style.Hero}>
+                    <p ref={textRef}>
 
-                {/* Panel Two */}
-                <div className={style.panelTwo}>
-                    <div className={style.ImageWrapper}>
-                        <Image src={image_1} alt="image_1" fill className={style.image} />
-                    </div>
-                </div>
+                    </p>
+                </section>
+                {/* Image*/}
+                <section className={style.Image}>
 
-                {/* Panel Three */}
-                <div className={style.panelThree}>
-                    <div className={style.ImageWrapper}>
-                        <Image src={image_2} alt="image_2" fill className={style.image} />
+                    <img src={image_1} alt={project.title} />
+
+                </section>
+
+                {/* Tech stack and description */}
+                <section className={style.TechStack}>
+                    <div className={style.description}>
+                        <h2>About the Project</h2>
+                        <p>{description}</p>
                     </div>
+
                     <div className={style.techStack}>
-                        {techStack.map((tech, index) => (
-                            <span key={index} className={style.techBadge}>{tech}</span>
-                        ))}
+                        <h2>Tech Stack</h2>
+                        <ul>
+                            {techStack.map((tech, index) => (
+                                <li key={index}>{tech}</li>
+                            ))}
+                        </ul>
                     </div>
-                </div>
+                </section>
+
+                {/* Key features */}
+                <section className={style.KeyFeatures}>
+
+                </section>
+
+
+
+                {/* Key features */}
+                <section className={style.KeyFeatures}>
+
+                </section>
+
+                {/* Links */}
+                <section className={style.Links}>
+                    {/* <button>Live</button>
+                    <button>GitHub</button> */}
+                </section>
             </div>
         </div>
     );
